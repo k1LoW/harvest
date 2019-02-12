031a1426552d9102acc06c96de11b71221854a92
k1LoW <k1lowxb@gmail.com>
2019-02-12 21:19:26 +0900

    Fix syslog timestamp regexp

--- parser/syslog.go
+++ parser/syslog.go
@@ -2,8 +2,8 @@ package parser
 
 // NewSyslogParser ...
 func NewSyslogParser() (Parser, error) {
-	r := `^(\w{3} \d{2} \d{2}:\d{2}:\d{2}) .+$`
-	tf := "Jan 02 15:04:05"
+	r := `^(\w{3}  ?\d{1,2} \d{2}:\d{2}:\d{2}) .+$`
+	tf := "Jan 2 15:04:05"
 	multiLine := false
 	return NewRegexpParser(r, tf, multiLine)
 }
