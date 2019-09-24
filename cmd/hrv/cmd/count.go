9c4ef0a352155b58de60a9e9876b2d71dc4bca13
k1LoW <k1lowxb@gmail.com>
2019-09-24 23:33:57 +0900

    mv

--- cmd/hrv/cmd/count.go
+++ cmd/hrv/cmd/count.go
@@ -30,13 +30,8 @@ import (
 // countCmd represents the count command
 var countCmd = &cobra.Command{
 	Use:   "count",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "count logs",
+	Long:  `count logs.`,
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("count called")
 	},
@@ -44,14 +39,4 @@ to quickly create a Cobra application.`,
 
 func init() {
 	rootCmd.AddCommand(countCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// countCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// countCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
